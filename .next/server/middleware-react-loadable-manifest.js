561b743bc99b1d1db76f66d6141579a027d9fb48
knowset <124893606+knowset@users.noreply.github.com>
2023-10-30 17:52:49 +0300

    Delete .next directory

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"../components/Map.tsx -> ./FindRoutesMap\":{\"id\":\"../components/Map.tsx -> ./FindRoutesMap\",\"files\":[\"static/chunks/_app-pages-browser_src_components_FindRoutesMap_tsx.js\"]}}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ZjeMcW+0BMNKiDu4OA0yn3XL2U5RJ/CA6vjQ0RnBs+U=\"\n}"
